5dbef1ada43ee85274f15aa7ee46c7336dd0bd2d
elegua <elegua@firewatcher.org>
2015-01-12 13:41:25 -0800

    added compatibility for existing Elixir Enumerables

--- lib/transducible.ex
+++ lib/transducible.ex
@@ -1,8 +1,27 @@
 defprotocol Transducible do
+  @fallback_to_any true
+  
   def next(collection)
+end
+
+defimpl Transducible, for: Any do
+  def next(enum) do
+    case Enumerable.reduce(enum, {:cont, nil}, fn elem, _ -> {:suspend, elem} end) do
+      {:suspended, next_elem, cont_fun} -> {next_elem, cont_fun}
+    end
+  end
 end
 
 defimpl Transducible, for: List do
   def next([elem | rem]), do: {elem, rem}
   def next([]), do: :empty
+end
+
+defimpl Transducible, for: Function do
+  def next(fun) do
+    case fun.({:cont, nil}) do
+      {:suspended, next_elem, cont_fun} -> {next_elem, cont_fun}
+      {:done, nil} -> :empty
+    end
+  end
 end
--- test/transducible_test.exs
+++ test/transducible_test.exs
@@ -5,4 +5,17 @@ defmodule TransducibleTest do
     assert Transducible.next([1,2,3]) == {1, [2,3]}
     assert Transducible.next([]) == :empty
   end
+
+  test "transducible ranges" do
+    assert {1, one} = Transducible.next(1..2)
+    assert {2, zero} = Transducible.next(one)
+    assert :empty == Transducible.next(zero)
+  end
+
+  test "transducible functions" do
+    suspend_fun = fn {:cont, _state} -> {:suspended, 1, :fun} end
+    assert {1, :fun} == Transducible.next(suspend_fun)
+    completed_fun = fn {:cont, _state} -> {:done, nil} end
+    assert :empty == Transducible.next(completed_fun)
+  end
 end
