fe2774ab53a5dd9fb8fae02e205585cc8ab63cab
elegua <elegua@firewatcher.org>
2015-01-31 08:35:26 -0800

    improved testing coverage for ET.Reducers.all?

--- test/et_reducers_test.exs
+++ test/et_reducers_test.exs
@@ -1,12 +1,33 @@
 defmodule ETReducersTest do
   use ExUnit.Case
 
-  test "all?" do
-    lt_four = ET.Reducers.all?(&(&1<4))
-    assert ET.reduce([1,2,3], lt_four) == true
-    assert ET.reduce([1,2,3,4], lt_four) == false
-    assert ET.reduce([true, true], ET.Reducers.all?) == true
-    assert ET.reduce([true, false, true], ET.Reducers.all?) == false
+  defp identity_trans, do: ET.Transducers.map(&(&1))
+
+  test "all?()" do
+    all_0_test(ET.Reducers.all?)
+  end
+
+  test "all?(transducer)" do
+    all_0_test(identity_trans |> ET.Reducers.all?)
+  end
+
+  defp all_0_test (reducer) do
+    assert ET.reduce([1, true], reducer) == true
+    assert ET.reduce([true, false, true], reducer) == false
+    assert ET.reduce([true, nil, true], reducer) == false
+  end
+
+  test "all?(check_fun)" do
+    all_1_test(ET.Reducers.all?(&(&1<4)))
+  end
+
+  test "all?(transducer, check_fun)" do
+    all_1_test(identity_trans |> ET.Reducers.all?(&(&1<4)))
+  end
+
+  defp all_1_test(reducer) do
+    assert ET.reduce([1,2,3], reducer) == true
+    assert ET.reduce([1,2,3,4], reducer) == false
   end
 
   test "any?" do
