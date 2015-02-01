1511787561bc899f30433ac17c63730828a698c6
elegua <elegua@firewatcher.org>
2015-01-31 16:01:40 -0800

    moved generic chunk from ET.Transducers to ET.Logic

--- lib/et/logic.ex
+++ lib/et/logic.ex
@@ -15,6 +15,104 @@ defmodule ET.Logic do
   """
 
   import ET.Transducer
+
+
+  @doc """
+  A transducer which applies each element to an arbitrary number of 
+  inner reducers. New inner reducers are generated on a {_, true} element.
+  Elements are applied to inner reducers in the order they were generated.
+  Inner reducers must :halt on their own and whenever the oldest inner
+  reducer is in :halt, it will be finished and passed to the main reducer.
+
+  If a padding transducible is provided, on finish, any remaining inner
+  reducers will be fed from that until they :halt (as above) or the
+  padding is :done. In this case, they will all be finished and sent to
+  the main reducer. Padding elements are mapped to {element, nil}.
+
+  If no padding transducible (nil) is provided, on :fin, all inner reducers
+  will be finished, but they will not be sent to the main reducer.
+
+  """
+  def chunk(inner_reducer), do: chunk(inner_reducer, nil)
+  def chunk(%ET.Transducer{} = trans, inner_reducer) do
+    compose(trans, chunk(inner_reducer))
+  end
+  def chunk(inner_reducer, padding) do
+    %ET.Transducer{elements: [fn reducer ->
+      fn :init ->
+           r_signal = {signal, _} = reducer.(:init)
+           r_signal |> prepend_state({signal, []})
+         {:cont, [{signal, chunks} | r_state], elem} ->
+           do_chunk(elem, chunks, inner_reducer, reducer, {signal, r_state})
+         {:fin, [{signal, chunks} | r_state]} ->
+           finish_chunk(chunks, inner_reducer, reducer, {signal, r_state}, padding)
+      end
+    end]}
+  end
+  def chunk(%ET.Transducer{} = trans, inner_reducer, padding) do
+    compose(trans, chunk(inner_reducer, padding))
+  end
+
+  defp do_chunk({_, bool} = elem, chunks, inner_reducer, outer_reducer, r_signal) when bool == false or bool == nil do
+    apply_element(elem, chunks, inner_reducer, outer_reducer, r_signal)
+  end
+  defp do_chunk(elem, chunks, inner_reducer, outer_reducer, r_signal) do
+    apply_element(elem, [inner_reducer.(:init) | chunks], inner_reducer, outer_reducer, r_signal)
+  end
+  
+  defp apply_element(elem, chunks, inner_reducer, outer_reducer, r_signal) do
+    apply_element(elem, :lists.reverse(chunks), inner_reducer, outer_reducer, r_signal, [])
+  end
+  defp apply_element(_, [], _, _, {signal, _} = r_signal, acc) do
+    r_signal |> prepend_state({signal, acc})
+  end
+  defp apply_element(elem, [chunk | chunks], inner_reducer, outer_reducer, {:halt, _} = r_signal, acc) do
+    apply_element(elem, chunks, inner_reducer, outer_reducer, r_signal, [chunk | acc])
+  end
+  defp apply_element(elem, [chunk | chunks], inner_reducer, outer_reducer, {:cont, r_state}, acc) do
+    c_signal =
+      case chunk do
+        {:cont, state} -> inner_reducer.({:cont, state, elem})
+        {:halt, _} -> chunk
+      end
+    case {c_signal, acc} do
+      {{:halt, state}, []} ->
+        r_elem = ET.finish_reduce(state, inner_reducer)
+        r_signal = outer_reducer.({:cont, r_state, r_elem})
+        apply_element(elem, chunks, inner_reducer, outer_reducer, r_signal, acc)
+      _ ->
+        apply_element(elem, chunks, inner_reducer, outer_reducer, {:cont, r_state}, [c_signal | acc])
+    end
+  end
+
+  defp finish_chunk(chunks, inner_reducer, outer_reducer, {signal, r_state}, padding) when signal == :halt or padding == nil do
+    finish_chunks(chunks, inner_reducer)
+    outer_reducer.({:fin, r_state})
+  end
+  defp finish_chunk(chunks, inner_reducer, outer_reducer, {:cont, _} = r_signal, padding) do
+    r_state = apply_padding(Transducible.next(padding), chunks, inner_reducer, outer_reducer, r_signal)
+    outer_reducer.({:fin, r_state})
+  end
+
+  defp finish_chunks(chunks, inner_reducer) do
+    chunks = :lists.reverse(chunks)
+    for {_, chunk} <- chunks, do: ET.finish_reduce(chunk, inner_reducer)
+  end
+
+  defp apply_padding(:done, chunks, inner_reducer, outer_reducer, r_signal) do
+    {_signal, r_state, _} = ET.reduce_elements(finish_chunks(chunks, inner_reducer), r_signal, outer_reducer)
+    r_state
+  end
+  defp apply_padding({elem, cont}, chunks, inner_reducer, outer_reducer, r_signal) do
+    case apply_element({elem, nil}, chunks, inner_reducer, outer_reducer, r_signal) do
+      {_, [{_, []} | r_state]}     -> r_state
+      {:halt,  [{_, chunks} | r_state]} ->
+        finish_chunks(chunks, inner_reducer)
+        r_state
+      {signal, [{_, chunks} | r_state]} ->
+        apply_padding(Transducible.next(cont), chunks, inner_reducer, outer_reducer, {signal, r_state})
+    end
+  end
   
   @doc """
   A transducer which reduces elements of form {_, false} and does not reduce
@@ -30,7 +128,7 @@ defmodule ET.Logic do
       fn :init -> reducer.(:init)
          {:cont, r_state, {_, bool}} when bool == false or bool == nil ->
            {:cont, r_state}
-         {:cont, r_state, _} = signal ->
+         {:cont, _, _} = signal ->
            reducer.(signal)
          {:fin, r_state} -> reducer.({:fin, r_state})
       end
--- test/et_logic_test.exs
+++ test/et_logic_test.exs
@@ -2,6 +2,51 @@ defmodule ETLogicTest do
   use ExUnit.Case, async: true
 
   defp identity_trans, do: ET.Transducers.map(&(&1))
+
+  test "ET.Logic.chunk(inner_reducer)" do
+    ET.Logic.chunk(ET.Transducers.take(2) |> ET.Reducers.list)
+    |> ET.Reducers.list
+    |> chunk_inner_reducer_test
+  end
+
+  test "ET.Logic.chunk(transducer, inner_reducer)" do
+    identity_trans
+    |> ET.Logic.chunk(ET.Transducers.take(2) |> ET.Reducers.list)
+    |> ET.Reducers.list
+    |> chunk_inner_reducer_test
+  end
+
+  defp chunk_inner_reducer_test(reducer) do
+    ET.reduce([{1, false}, {2, true}, {3, false}, {4, true}], reducer)
+    [[{2, true}, {3, false}]]
+  end
+
+  test "ET.Logic.chunk(inner_reducer, padding)" do
+    ET.Logic.chunk((ET.Transducers.take(2) |> ET.Reducers.list), [])
+    |> ET.Reducers.list
+    |> chunk_inner_reducer_padding_test
+  end
+
+  test "ET.Logic.chunk(transducer, inner_reducer, padding)" do
+    identity_trans
+    |> ET.Logic.chunk((ET.Transducers.take(2) |> ET.Reducers.list), [])
+    |> ET.Reducers.list
+    |> chunk_inner_reducer_padding_test
+  end
+
+  defp chunk_inner_reducer_padding_test(reducer) do
+    assert ET.reduce([{1, false}, {2, true}, {3, false}, {4, true}], reducer) ==
+    [[{2, true}, {3, false}], [{4, true}]]
+  end
+
+  test "ET.Logic.chunk(inner_reducer, padding) with extra padding" do
+    reducer =
+      ET.Logic.chunk((ET.Transducers.take(2) |> ET.Reducers.list), 5..8)
+      |> ET.Reducers.list
+      
+    assert ET.reduce([{1, false}, {2, true}, {3, false}, {4, true}], reducer) == 
+    [[{2, true}, {3, false}], [{4, true}, {5, nil}]]    
+  end
   
   test "ET.Logic.filter()" do
     ET.Logic.filter
